cb8b3dcf9ecff7f8a0b2e33dee7409acfda6a650
Sean Ransonette <sransonette@gmail.com>
2020-08-14 23:19:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -38,12 +38,25 @@ class Artist
     @@all
   end
   
+<<<<<<< HEAD
   def self.find_or_create_by_name(artist_name)
     if self.find(artist_name)
       self.find(artist_name) 
     else 
       self.new(artist_name)
     end
+=======
+  def self.find_or_create_by_name(song)
+  artist = self.all.find {|artist| artist.name == name}
+<<<<<<< HEAD
+  if artist
+=======
+  if song = artist.name 
+>>>>>>> 5cddaf2920221937c4e1e1271a498f94690ab24a
+    artist 
+  else 
+    name = self.new(name)
+>>>>>>> e7ab82d702042393a5725913c6d23256b0f991a0
   end
   
   def self.find(name)
